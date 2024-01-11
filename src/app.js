0c7834858503f17dcbf236e2c99564c225b9eba7
Abhishek p p <abhishekkanichery@gmail.com>
2024-01-11 13:17:41 +0530

    implements register-api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connection = require("./connection");
+const regRoutes = require("./routes/registerRoutes");
 
 //Connect to DB
 connection.connectToDb;
@@ -9,6 +10,9 @@ connection.connectToDb;
 // Body parser middleware
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//Routes middlewares
+app.use("/api/user", regRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Running at ${PORT}`);
---
+++ src/routes/registerRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const User = require("../models/userSchema");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const saltRound = 10;
+
+//Sign up GET routes
+router.get("/signup", (req, res) => {
+  res.send("Welcome to signup");
+});
+
+//Sign up POST routes
+router.post("/signup", async (req, res) => {
+  try {
+    //get all the data from the request body
+    let { email, password } = req.body;
+    //All data should be there
+    if (!(email, password)) {
+      res.status(400).send("All fields are compulsory!");
+    } else {
+      //check if user already exist email
+      const existingUser = await User.findOne({ email: email });
+      if (existingUser) {
+        res.status(400).send("Email already exists!");
+      } else {
+        //Encrypt the password
+        const hashedPassword = await bcrypt.hash(password, saltRound);
+        //save the user to db
+        const user = await User.create({
+          email: email,
+          password: hashedPassword,
+        });
+        res.status(200).send({ user });
+        res.send("Registered Sucessfully");
+      }
+    }
+  } catch {
+    console.log(Error);
+  }
+});
+
+module.exports = router;
